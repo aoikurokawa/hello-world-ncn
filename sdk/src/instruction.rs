789df49ecec0d334ceb1cff2876a44d38a58826f
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-03-02 16:56:34 +0900

    Initialize Config Instruction (#7)

---
+++ sdk/src/instruction.rs
@@ -0,0 +1,11 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use shank::ShankInstruction;
+
+#[derive(Debug, BorshSerialize, BorshDeserialize, ShankInstruction)]
+pub enum HelloWorldNcnInstruction {
+    /// Initializes global configuration
+    #[account(0, writable, name = "config_info")]
+    #[account(1, writable, signer, name = "ncn_admin_info")]
+    #[account(2, name = "system_program_info")]
+    InitializeConfig,
+}
