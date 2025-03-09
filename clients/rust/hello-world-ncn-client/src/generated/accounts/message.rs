0fbd6cf31a3f8255d900a1360c5214daea331d5e
aoikurokawa <ht.so.ik0102@gmail.com>
2025-03-10 06:42:21 +0900

    fix: fmt

--- clients/rust/hello-world-ncn-client/src/generated/accounts/message.rs
+++ clients/rust/hello-world-ncn-client/src/generated/accounts/message.rs
@@ -5,67 +5,65 @@
 //! <https://github.com/codama-idl/codama>
 //!
 
-use solana_program::pubkey::Pubkey;
-use borsh::BorshSerialize;
 use borsh::BorshDeserialize;
-
+use borsh::BorshSerialize;
+use solana_program::pubkey::Pubkey;
 
 #[derive(BorshSerialize, BorshDeserialize, Clone, Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Message {
-pub discriminator: u64,
-#[cfg_attr(feature = "serde", serde(with = "serde_with::As::<serde_with::DisplayFromStr>"))]
-pub ncn: Pubkey,
-pub epoch: u64,
-pub keyword_len: u8,
-#[cfg_attr(feature = "serde", serde(with = "serde_with::As::<serde_with::Bytes>"))]
-pub keyword: [u8; 64],
+    pub discriminator: u64,
+    #[cfg_attr(
+        feature = "serde",
+        serde(with = "serde_with::As::<serde_with::DisplayFromStr>")
+    )]
+    pub ncn: Pubkey,
+    pub epoch: u64,
+    pub keyword_len: u8,
+    #[cfg_attr(feature = "serde", serde(with = "serde_with::As::<serde_with::Bytes>"))]
+    pub keyword: [u8; 64],
 }
 
-
 impl Message {
-  
-  
-  
-  #[inline(always)]
-  pub fn from_bytes(data: &[u8]) -> Result<Self, std::io::Error> {
-    let mut data = data;
-    Self::deserialize(&mut data)
-  }
+    #[inline(always)]
+    pub fn from_bytes(data: &[u8]) -> Result<Self, std::io::Error> {
+        let mut data = data;
+        Self::deserialize(&mut data)
+    }
 }
 
 impl<'a> TryFrom<&solana_program::account_info::AccountInfo<'a>> for Message {
-  type Error = std::io::Error;
+    type Error = std::io::Error;
 
-  fn try_from(account_info: &solana_program::account_info::AccountInfo<'a>) -> Result<Self, Self::Error> {
-      let mut data: &[u8] = &(*account_info.data).borrow();
-      Self::deserialize(&mut data)
-  }
+    fn try_from(
+        account_info: &solana_program::account_info::AccountInfo<'a>,
+    ) -> Result<Self, Self::Error> {
+        let mut data: &[u8] = &(*account_info.data).borrow();
+        Self::deserialize(&mut data)
+    }
 }
 
-  #[cfg(feature = "anchor")]
-  impl anchor_lang::AccountDeserialize for Message {
-      fn try_deserialize_unchecked(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
+#[cfg(feature = "anchor")]
+impl anchor_lang::AccountDeserialize for Message {
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> anchor_lang::Result<Self> {
         Ok(Self::deserialize(buf)?)
-      }
-  }
+    }
+}
 
-  #[cfg(feature = "anchor")]
-  impl anchor_lang::AccountSerialize for Message {}
+#[cfg(feature = "anchor")]
+impl anchor_lang::AccountSerialize for Message {}
 
-  #[cfg(feature = "anchor")]
-  impl anchor_lang::Owner for Message {
-      fn owner() -> Pubkey {
+#[cfg(feature = "anchor")]
+impl anchor_lang::Owner for Message {
+    fn owner() -> Pubkey {
         crate::HELLO_WORLD_NCN_ID
-      }
-  }
+    }
+}
 
-  #[cfg(feature = "anchor-idl-build")]
-  impl anchor_lang::IdlBuild for Message {}
+#[cfg(feature = "anchor-idl-build")]
+impl anchor_lang::IdlBuild for Message {}
 
-  
-  #[cfg(feature = "anchor-idl-build")]
-  impl anchor_lang::Discriminator for Message {
+#[cfg(feature = "anchor-idl-build")]
+impl anchor_lang::Discriminator for Message {
     const DISCRIMINATOR: [u8; 8] = [0; 8];
-  }
-
+}
